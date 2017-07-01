2c15e3de73bade7fae30e172b99890a54f2e5d6f
Ron Lisle <ron@lisles.net>
2017-07-01 09:46:46 -0500

    Initial Commit

---
+++ Settings/Settings.h
@@ -0,0 +1,19 @@
+//
+//  Settings.h
+//  Settings
+//
+//  Created by Ron Lisle on 7/1/17.
+//  Copyright © 2017 Ron Lisle. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Settings.
+FOUNDATION_EXPORT double SettingsVersionNumber;
+
+//! Project version string for Settings.
+FOUNDATION_EXPORT const unsigned char SettingsVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Settings/PublicHeader.h>
+
+
